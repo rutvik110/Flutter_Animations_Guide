ff61ce5fb936066f84c4e4eb42bea69b765c99c8
rutvik <takrutvik@gmail.com>
2020-12-10 10:45:01 +0530

    So it begins!!!

---
+++ flutter_animations_examples/TomnJerryAnimation/lib/animationcontroller.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AnimationControllerSilder extends StatefulWidget {
+  AnimationController animationController;
+  Function buildaniamtion;
+  Function playanimation;
+  AnimationControllerSilder({this.animationController,this.buildaniamtion,this.playanimation});
+  @override
+  _AnimationControllerSilderState createState() => _AnimationControllerSilderState();
+}
+
+class _AnimationControllerSilderState extends State<AnimationControllerSilder> {
+  bool isplaying = false;
+  @override
+  Widget build(BuildContext context) {
+    widget.animationController.addStatusListener((status) { 
+
+      if(status == AnimationStatus.completed){
+        isplaying = false;
+      }
+    });
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: isplaying ? Icon(Icons.pause,color: Color(0xFF9DF7F7),) : Icon(Icons.play_arrow,color: Color(0xFF9DF7F7),),
+          onPressed: (){
+              isplaying = !isplaying;
+            widget.playanimation();
+          },
+        ),
+        Slider(
+          min: 0.0,
+          max: 1.0,
+          value: widget.animationController.value,
+          activeColor: Color(0xFF9DF7F7),
+          inactiveColor: Color(0xFFC2E8E8).withOpacity(0.9),
+          onChanged: (value){
+            print(value);
+          
+            widget.buildaniamtion();
+            widget.animationController.value = value;
+          },
+          
+        )
+      ],
+      
+    );
+  }
+}
